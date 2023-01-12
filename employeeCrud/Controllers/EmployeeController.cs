ea1a1db590b624c43307391fb114edea1eb60e15
Murtuza Vhora <Murtuza Vhora@DESKTOP-AMT9NMC>
2023-01-12 18:03:52 +0530

    Add project files.

---
+++ employeeCrud/Controllers/EmployeeController.cs
@@ -0,0 +1,63 @@
+﻿using employeeCrud.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace employeeCrud.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+         private CompanyContext _companyContext;
+        public EmployeeController(CompanyContext companyContext)
+        {
+            _companyContext = companyContext;
+        }
+        // GET: api/<EmployeeController>
+        [HttpGet]
+        public IEnumerable<Employee> Get()
+        {
+            return _companyContext.Employees;
+        }
+
+        // GET api/<EmployeeController>/5
+        [HttpGet("{id}")]
+        public Employee Get(int id)
+        {
+            return _companyContext.Employees.FirstOrDefault(s => s.EmployeeId == id);
+        }
+
+        // POST api/<EmployeeController>
+        [HttpPost]
+        public void Post([FromBody] Employee value)
+        {
+            _companyContext.Employees.Add(value);
+            _companyContext.SaveChanges();
+        }
+
+        // PUT api/<EmployeeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Employee value)
+        {
+            var employee = _companyContext.Employees.FirstOrDefault(s => s.EmployeeId == id);
+            if (employee != null)
+            {
+                _companyContext.Entry<Employee>(employee).CurrentValues.SetValues(value);
+                _companyContext.SaveChanges();
+            }
+        }
+
+        // DELETE api/<EmployeeController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            var student = _companyContext.Employees.FirstOrDefault(s => s.EmployeeId == id);
+            if (student != null)
+            {
+                _companyContext.Employees.Remove(student);
+                _companyContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ employeeCrud/Models/CompanyContext.cs
@@ -0,0 +1,20 @@
+﻿namespace employeeCrud.Models
+{
+ 
+    using employeeCrud.Models;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+
+
+    public class CompanyContext
+        : DbContext
+    {
+        public CompanyContext(DbContextOptions options)
+        : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
